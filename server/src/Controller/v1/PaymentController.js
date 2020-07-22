328b69a27f981add2bda74fe0937d81ce23e55a4
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-07-23 00:07:05 +0530

    add date

--- server/src/Controller/v1/PaymentController.js
+++ server/src/Controller/v1/PaymentController.js
@@ -189,7 +189,7 @@ module.exports = {
 		const types = parseInt(type) === 3 ? 'date' : 'created';
 		let conditions = `where user_id = ${user_id} and ${types} > ${app.dateToUnixTime(
 			from_date
-		)} and ${types} < ${app.dateToUnixTime(to_date)}`;
+		)} and ${types} < ${app.dateToUnixTime(to_date) + 86400}`;
 		conditions +=
 			parseInt(type) !== 0 ? ` and type = ${type}` : ` and type in (1,2)`;
 		const result = await DB.first(

10b48111e9a91d08cdddf86ebc4bfb4cad335090
pankajvashisht-ucreate <iampankajvashisht@gmail.com>
2023-11-24 20:28:29 +0530

    fixed the issue

--- src/Modules/Web/Login/index.vue
+++ src/Modules/Web/Login/index.vue
@@ -85,7 +85,7 @@ export default {
         .then((responce) => {
           const { data } = responce;
           localStorage.setItem("usersInfo", JSON.stringify(data.data));
-          this.returnUrl = this.$route.query.returnUrl || "/profile";
+          this.returnUrl = this.$route.query.returnUrl || "/user/profile";
           this.$router.push(this.returnUrl);
           this.loading = false;
         })
--- src/Modules/Web/index.js
+++ src/Modules/Web/index.js
@@ -10,7 +10,7 @@ export default [
     },
   },
   {
-    path: "/profile",
+    path: "/user/profile",
     name: "profile",
     component: () => import(/* webpackChunkName: dashboard" */ "./Profile"),
     meta: {
